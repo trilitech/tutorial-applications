3ea93485b4bac47d6b8faf17562e09f3fb405479
Benjamin Fuentes <zamrokk@gmail.com>
2024-10-30 11:20:30 +0100

    next

---
+++ training-dapp-4/app/src/ConnectWallet.tsx
@@ -0,0 +1,49 @@
+import { NetworkType } from '@airgap/beacon-sdk';
+import { BeaconWallet } from '@taquito/beacon-wallet';
+import { TezosToolkit } from '@taquito/taquito';
+import { Dispatch, SetStateAction } from 'react';
+type ButtonProps = {
+    Tezos: TezosToolkit;
+    setUserAddress: Dispatch<SetStateAction<string>>;
+    setUserBalance: Dispatch<SetStateAction<number>>;
+    wallet: BeaconWallet;
+    setTezos: Dispatch<SetStateAction<TezosToolkit>>;
+};
+const ConnectButton = ({
+    Tezos,
+    setTezos,
+    setUserAddress,
+    setUserBalance,
+    wallet,
+}: ButtonProps): JSX.Element => {
+    const connectWallet = async (): Promise<void> => {
+        try {
+            await wallet.requestPermissions({
+                network: {
+                    type: NetworkType.GHOSTNET,
+                    rpcUrl: 'https://ghostnet.ecadinfra.com',
+                },
+            });
+            // gets user's address
+            const userAddress = await wallet.getPKH();
+            const balance = await Tezos.tz.getBalance(userAddress);
+            setUserBalance(balance.toNumber());
+            setUserAddress(userAddress);
+
+            Tezos.setWalletProvider(wallet);
+            setTezos(Tezos);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    return (
+        <div className="buttons">
+            <button className="button" onClick={connectWallet}>
+                <span>
+                    <i className="fas fa-wallet"></i>&nbsp; Connect with wallet
+                </span>
+            </button>
+        </div>
+    );
+};
+export default ConnectButton;
