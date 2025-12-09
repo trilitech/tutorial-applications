92dcd3f5cacdf6dc74297617b153a8da6d449174
Tim McMackin <timothymcmackin@yahoo.com>
2025-12-09 09:38:26 -0500

    Use local image

--- etherlink-marketpulse/app/src/App.tsx
+++ etherlink-marketpulse/app/src/App.tsx
@@ -410,7 +410,7 @@ export default function App({ thirdwebClient }: AppProps) {
         <div style={{ width: "calc(66vw - 4rem)" }}>
           <img
             style={{ maxHeight: "40vh" }}
-            src="https://zamrokk.github.io/marketpulse/images/graph.png"
+            src="graph.png"
           />
           <hr />
 
@@ -431,7 +431,6 @@ export default function App({ thirdwebClient }: AppProps) {
                         <img
                           style={{ objectFit: "cover", height: "inherit" }}
                           src={
-                            "https://zamrokk.github.io/marketpulse/images/" +
                             option +
                             ".png"
                           }
